f1a06c4501340f30a95d22fb71e725c00572c08e
rafalos <rsulkowski1@gmail.com>
2023-10-09 10:18:26 +0200

    add initial styling and markup for details module

--- src/app/components/Register/Details.tsx
+++ src/app/components/Register/Details.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { FaRegClock } from 'react-icons/fa';
+import { FaRegCalendar } from 'react-icons/fa';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import styles from './Details.module.scss'
 
 type Props = {};
 
 const Details = (props: Props) => {
   return (
     <>
+      
       <h2>- Register Here -</h2>
       <p>Join us to celebrate our biggest night of the year</p>
-      <div>
-        <div>05 NOVEMBER 2022</div>
-        <div>16:00 – 23:00</div>
-        <div>Centara Grand & Bangkok Convention Centre, Bangkok</div>
+      <div className={styles.info}>
+        <div className={styles['info__box']}>
+          <FaMapMarkerAlt />
+          <p>05 NOVEMBER 2022</p>
+        </div>
+        <div className={styles['info__box']}>
+          <FaRegClock />
+          <p>16:00 – 23:00</p>
+        </div>
+        <div className={styles['info__box']}>
+          <FaRegCalendar />
+          <p>Centara Grand & Bangkok Convention Centre, Bangkok</p>
+        </div>
       </div>
     </>
   );
