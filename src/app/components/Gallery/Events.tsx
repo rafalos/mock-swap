274ca9fe8e423253af6ae1de7d5efc6dec22e34c
rafalos <rsulkowski1@gmail.com>
2023-10-11 02:49:16 +0200

    fix responsivness on the mobile

--- src/app/components/Gallery/Events.tsx
+++ src/app/components/Gallery/Events.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Gallery.module.scss';
 import { FaChevronLeft } from 'react-icons/fa';
 import { FaChevronRight } from 'react-icons/fa';
@@ -9,22 +9,20 @@ import useIsMobile from '@/app/hooks/useIsMobile';
 import useIsTabet from '@/app/hooks/useIsTablet';
 
 const Events = () => {
-  const isMobile = useIsMobile();
-  const isTablet = useIsTabet();
-
-  const getCount = () => {
-    if (isMobile) return 2;
-    if (isTablet) return 4;
-    return 5;
-  };
-
-  const toDisplay = events.slice(0, getCount());
+  const toDisplay = events.slice(0, 5);
 
   return (
     <div className={styles['gallery__events']}>
       <FaChevronLeft />
       {toDisplay.map((ev) => (
-        <Image src={ev.image} alt={ev.alt} key={ev.alt} height={128} width={128}/>
+        <Image
+          className={styles.event}
+          src={ev.image}
+          alt={ev.alt}
+          key={ev.alt}
+          height={114}
+          width={114}
+        />
       ))}
       <FaChevronRight />
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'XM Task',
   description: 'XM task site',
+  
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
