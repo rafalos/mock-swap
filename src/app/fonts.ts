1a62f1da8a1740ae5c6a66b14bfc66046c7b00b6
Rafal Sulkowski <rafal.sulkowski@accenture.com>
2023-10-13 17:51:01 +0200

    fix roboto font not having proper weight properties

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,12 +1,12 @@
-import { Bebas_Neue, Roboto_Mono } from 'next/font/google';
+import { Bebas_Neue, Roboto } from 'next/font/google';
+
+export const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export const berbasNeue = Bebas_Neue({
   subsets: ['latin'],
   weight: ['400'],
-});
-
-export const roboto = Roboto_Mono({
-  weight: ['300', '400', '500', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
 });
