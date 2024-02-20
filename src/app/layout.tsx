128d422fb9a4f7c7f1218a2e06370dddcb2a2366
rafalos <rsulkowski1@gmail.com>
2024-02-21 00:09:57 +0100

    fix conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,9 @@ import './globals.scss';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-<<<<<<< HEAD
-  title: 'XM Task',
-  description: 'XM task site',
-=======
   title: 'MockSwap',
   description: 'MockSwap - here the journey begins',
   
->>>>>>> 2f582e4 (rename the app to MockSwap)
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
