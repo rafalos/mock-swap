e7a81777bc283b9afe2b4e8c9db05f41332c4bd5
rafalos <rsulkowski1@gmail.com>
2024-02-21 00:32:42 +0100

    remove branding

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <nav className={styles.navbar}>
-      <Image src='/logo.png' alt='MockSwap Logo' width={140} height={50} />
+      <h2>MockSwap</h2>
       <ul className={styles['navbar__links']}>
         <li>MockSwap Homepage</li>
         <li>Support</li>
